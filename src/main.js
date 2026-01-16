6632219510e7ff6cc460ba374f3a1e311a652641
LongTang <long.tang628@gmail.com>
2026-01-16 18:40:34 -0500

    Library-002:_Displaying_Books

--- src/main.js
+++ src/main.js
@@ -10,7 +10,41 @@ class Book {
     }
 }
 
-function addBookToLibrary( title, author, pages ) {
-    const newBook = new Book(title, author, pages );
-    library.push( newBook );
+function addBookToLibrary(title, author, pages) {
+    const newBook = new Book(title, author, pages);
+    library.push(newBook);
 }
+
+function displayBooks() {
+    const libraryContainer = document.querySelector("#library-container");
+
+    // Wiping the UI clean before redrawing
+    libraryContainer.innerHTML = "";
+
+    library.forEach((book) => {
+        // Create a new DOM element (the "Card" )
+        const bookCard = document.createElement("div");
+        bookCard.classList.add("book-card");
+
+        // Attach the ID to the HTML (The "barcode")
+        bookCard.dataset.bookId = book.id;
+
+        // Set the content
+        bookCard.innerHTML = `
+            <h3>${book.title}</h3>
+            <p>By: ${book.author}</p>
+            <p>Pages: ${book.pages}</p>
+            <p>Status: ${book.isRead ? "Read" : "Not Read"}</p>
+            <button class="remove-btn">Remove</button>
+            <button class="toggle-btn">Change Status</button>
+        `;
+
+        libraryContainer.appendChild(bookCard);
+    });
+}
+
+addBookToLibrary("The Hobbit", "J.R.R. Tolkien", 295);
+addBookToLibrary("1984", "George Orwell", 328);
+addBookToLibrary("The Great Gatsby", "F. Scott Fitzgerald", 180);
+
+displayBooks();
