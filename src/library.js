78667617d5d323617318d7102b6e2124baefcb56
LongTang <long.tang628@gmail.com>
2026-01-17 15:47:34 -0500

    Library-006(refactor): encapsulate book data and methods into Library module

--- src/library.js
+++ src/library.js
@@ -1,19 +1,33 @@
+// The Blueprint
+class Book {
+    constructor(title, author, pages, isRead) {
+        this.id = crypto.randomUUID();
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.isRead = isRead;
+    }
+
+    toggleReadStatus() {
+        this.isRead = !this.isRead;
+    }
+}
+
+// The Storage
 export const Library = {
     books: [],
 
     addBook(title, author, pages ,isRead) {
-        const book = { 
-            id: crypto.randomUUID(),
-            title,
-            author,
-            pages,
-            isRead
-        };
-        
-        this.books.push( book );
+        // Use the 'new' keyword to create an instance with methods
+        const newBook = new Book(title, author, pages, isRead);
+        this.books.push( newBook );
     },
 
     removeBook( id ) {
         this.books = this.books.filter(book => book.id !== id);
+    },
+
+    getBook( id ) {
+        return this.books.find(book => book.id === id);
     },
 }
